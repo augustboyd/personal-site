e4f707528a5e869245944cb769be6283102e3ad2
acboyd1 <83028175+acboyd1@users.noreply.github.com>
2024-06-12 15:47:16 -0400

    added a navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
